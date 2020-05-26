c0b96ecd13be6a56b45de2fba2cc59a30391ec01
Rahul <52566009+Rahul-9999@users.noreply.github.com>
2020-05-26 23:39:09 +0530

    Add files via upload

---
+++ 10_JQuery_Magnify_Plugin/10_app.js
@@ -0,0 +1,9 @@
+$('.zoomple').zoomple({
+    blankURL : 'images/blank.png',
+    bgColor : '#90D5D9',
+    loaderURL : 'images/loader.gif',
+    offset : {x:-150,y:-150},
+    zoomWidth : 200,
+    zoomHeight : 200,
+    roundedCorners : true
+});
